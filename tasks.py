b624053f1b945b494bd42191118c6d3025a5d66f
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-10-29 00:14:29 +0100

    colorize output of copy

--- tasks.py
+++ tasks.py
@@ -1,32 +1,50 @@
-from abc import abstractmethod, abstractproperty
+from abc import abstractmethod
+from collections import defaultdict
 from dataclasses import dataclass, field
+from itertools import chain
 from pathlib import Path
 from typing import Iterable
 
 from invoke import task
+from rich import print
+from rich.text import Text
 
 import config
 
 
+@dataclass
 class Command:
+    sudo: bool = field(default=False, kw_only=True)
+
     @abstractmethod
     def elements(self) -> Iterable[str | None | Path]: ...
 
     @property
     @abstractmethod
-    def describe(self) -> str: ...
+    def describe(self) -> str | Text: ...
+
+    def prefix(self) -> list[str]:
+        if self.sudo:
+            return ["sudo"]
+        else:
+            return []
 
     def __str__(self):
-        return " ".join(map(str, filter(None, self.elements())))
+        return " ".join(map(str, filter(None, chain(self.prefix(), self.elements()))))
+
+
+styles = defaultdict(lambda: "", {"path": "blue", "host": "green"})
 
 
 @dataclass
 class EnsureDirectoryExists(Command):
-    directory: str
+    directory: str | Path
 
     @property
     def describe(self):
-        return f"ensure directory {self.directory} exists"
+        return Text.assemble(
+            "ensure directory ", (str(self.directory), styles["path"]), " exists"
+        )
 
     def elements(self):
         return ["mkdir", "-p", self.directory]
@@ -34,12 +52,17 @@ class EnsureDirectoryExists(Command):
 
 @dataclass
 class ExecuteOnHost(Command):
-    host: str
+    host: str | Path
     command: Command
 
     @property
     def describe(self):
-        return f"run on host {self.host}: {self.command.describe}"
+        return Text.assemble(
+            "run on host ",
+            (str(self.host), styles["host"]),
+            ": ",
+            self.command.describe,
+        )
 
     def elements(self):
         return ["ssh", self.host, f'"{self.command}"']
@@ -47,13 +70,20 @@ class ExecuteOnHost(Command):
 
 @dataclass
 class CopyDirectoryAs(Command):
-    source: str
+    source: str | Path
     host: str
-    dest: str
+    dest: str | Path
 
     @property
     def describe(self):
-        return f"copy directory {self.source} on {self.host} as {self.dest}"
+        return Text.assemble(
+            "copy directory ",
+            (str(self.source), styles["path"]),
+            " on ",
+            (self.host, styles["host"]),
+            " as ",
+            (str(self.dest), styles["path"]),
+        )
 
     def elements(self):
         source_dir = Path(self.source).expanduser()
@@ -62,13 +92,20 @@ class CopyDirectoryAs(Command):
 
 @dataclass
 class CopyFileIn(Command):
-    source: str
+    source: str | Path
     host: str
-    dest: str
+    dest: str | Path
 
     @property
     def describe(self):
-        return f"copy file {self.source} on {self.host} in {self.dest}"
+        return Text.assemble(
+            "copy file ",
+            (str(self.source), styles["path"]),
+            " on ",
+            (self.host, styles["host"]),
+            " in ",
+            (str(self.dest), styles["path"]),
+        )
 
     def elements(self):
         source = Path(self.source).expanduser()
@@ -81,7 +118,9 @@ class DockerComposeUp(Command):
 
     @property
     def describe(self):
-        return f"start docker composition from file {self.file}"
+        return Text.assemble(
+            "start docker composition from file ", (self.file, styles["path"])
+        )
 
     def elements(self):
         file = Path(self.file).expanduser()
@@ -94,17 +133,25 @@ class DockerComposeDown(Command):
 
     @property
     def describe(self):
-        return f"terminate docker composition from file {self.file}"
+        return Text.assemble(
+            "terminate docker composition from file ", (self.file, styles["path"])
+        )
 
     def elements(self):
         file = Path(self.file).expanduser()
         return ["docker", "compose", "--file", file, "down"]
 
 
+def run_command(ctx, command):
+    print(command.describe)
+    ctx.run(str(command), echo=True)
+
+
 @task
 def copy(ctx):
     """Copy files on the server"""
     for source, mode, dest in config.content:
+        dest = Path(dest)
         match mode:
             case "in":
                 commands = [
@@ -112,23 +159,25 @@ def copy(ctx):
                     CopyFileIn(source, config.server, dest),
                 ]
             case "as":
-                commands = [CopyDirectoryAs(source, config.server, dest)]
+                commands = [
+                    ExecuteOnHost(config.server, EnsureDirectoryExists(dest.parent)),
+                    CopyDirectoryAs(source, config.server, dest),
+                ]
             case _:
                 raise RuntimeError(f"unknown mode: {mode}")
         for command in commands:
-            print(command.describe)
-            ctx.run(str(command), echo=True)
+            run_command(ctx, command)
 
 
-@task
-def terminate(ctx):
-    """Terminate server"""
-    command = ExecuteOnHost(config.server, DockerComposeDown(config.compose))
-    ctx.run(str(command))
+# @task
+# def terminate(ctx):
+#     """Terminate server"""
+#     command = ExecuteOnHost(config.server, DockerComposeDown(config.compose))
+#     run_command(ctx, command)
 
 
-@task
-def start(ctx):
-    """Start server"""
-    command = ExecuteOnHost(config.server, DockerComposeUp(config.compose))
-    ctx.run(str(command))
+# @task
+# def start(ctx):
+#     """Start server"""
+#     command = ExecuteOnHost(config.server, DockerComposeUp(config.compose))
+#     run_command(ctx, command)
