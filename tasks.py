1f5891e74feeec7dca4347520854056f5435a6a9
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-10-30 20:38:22 +0100

    refactor and change task

--- tasks.py
+++ tasks.py
@@ -1,209 +1,19 @@
-from abc import abstractmethod
-from collections import defaultdict
-from dataclasses import dataclass, field
-from itertools import chain
 from pathlib import Path
-from typing import Iterable
 
 from invoke import task
 from rich import print
-from rich.text import Text
 
 import config
-
-
-@dataclass
-class Command:
-    @abstractmethod
-    def elements(self) -> Iterable[str | None | Path]: ...
-
-    @property
-    @abstractmethod
-    def describe(self) -> str | Text: ...
-
-    def __str__(self):
-        return " ".join(map(str, filter(None, self.elements())))
-
-
-styles = defaultdict(lambda: "", {"path": "blue", "host": "green"})
-
-
-@dataclass
-class Sudo(Command):
-    command: Command
-
-    @property
-    def describe(self) -> str | Text:
-        return Text.assemble("Execute as Super User: ", self.command.describe)
-
-    def elements(self) -> Iterable[str | None | Path]:
-        return chain(["sudo"], self.command.elements())
-
-
-@dataclass
-class EnsureDirectoryExists(Command):
-    directory: str | Path
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "ensure directory ", (str(self.directory), styles["path"]), " exists"
-        )
-
-    def elements(self):
-        return ["mkdir", "-p", self.directory]
-
-
-@dataclass
-class ExecuteOnHost(Command):
-    host: str | Path
-    command: Command
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "run on host ",
-            (str(self.host), styles["host"]),
-            ": ",
-            self.command.describe,
-        )
-
-    def elements(self):
-        return ["ssh", self.host, f'"{self.command}"']
-
-
-@dataclass
-class CopyFileToRemote(Command):
-    source: str | Path
-    host: str
-    dest: str | Path
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "copy file ",
-            (str(self.source), styles["path"]),
-            " on host ",
-            (self.host, styles["host"]),
-            " in ",
-            (str(self.dest), styles["path"]),
-        )
-
-    def elements(self):
-        source = Path(self.source).expanduser()
-        return [
-            "scp",
-            source,
-            f"{self.host}:{self.dest}",
-        ]
-
-
-@dataclass
-class CopyDirToRemote(Command):
-    source: str | Path
-    host: str
-    dest: str | Path
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "copy directory ",
-            (str(self.source), styles["path"]),
-            " on ",
-            (self.host, styles["host"]),
-            " in ",
-            (str(self.dest), styles["path"]),
-        )
-
-    def elements(self):
-        source = Path(self.source).expanduser()
-        return [
-            "scp",
-            "-r",
-            source,
-            f"{self.host}:{self.dest}",
-        ]
-
-
-@dataclass
-class CopyDirContentToRemote(Command):
-    source: str | Path
-    host: str
-    dest: str | Path
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "copy content of dir ",
-            (str(self.source), styles["path"]),
-            " on ",
-            (self.host, styles["host"]),
-            " in ",
-            (str(self.dest), styles["path"]),
-        )
-
-    def elements(self):
-        source = Path(self.source).expanduser()
-        return [
-            "scp",
-            "-r",
-            str(source / "*"),
-            f"{self.host}:{self.dest}",
-        ]
-
-
-@dataclass
-class CopyDirContent(Command):
-    source: str | Path
-    dest: str | Path
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "copy content of dir ",
-            (str(self.source), styles["path"]),
-            " in ",
-            (str(self.dest), styles["path"]),
-        )
-
-    def elements(self):
-        source = Path(self.source).expanduser()
-        return [
-            "cp",
-            "-r",
-            str(source / "*"),
-            self.dest,
-        ]
-
-
-@dataclass
-class DockerComposeUp(Command):
-    file: str
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "start docker composition from file ", (self.file, styles["path"])
-        )
-
-    def elements(self):
-        file = Path(self.file).expanduser()
-        return ["docker", "compose", "--file", file, "up", "--detach"]
-
-
-@dataclass
-class DockerComposeDown(Command):
-    file: str
-
-    @property
-    def describe(self):
-        return Text.assemble(
-            "terminate docker composition from file ", (self.file, styles["path"])
-        )
-
-    def elements(self):
-        file = Path(self.file).expanduser()
-        return ["docker", "compose", "--file", file, "down"]
+from commands import (
+    CopyDirContent,
+    CopyDirContentToRemote,
+    CopyFile,
+    CopyFileToRemote,
+    EnsureDirectoryExists,
+    ExecuteOnHost,
+    NginxReload,
+    Sudo,
+)
 
 
 def run_command(ctx, command):
@@ -211,47 +21,56 @@ def run_command(ctx, command):
     ctx.run(str(command), echo=True)
 
 
-def copy_command_from_config(type_, source, dest):
-    match type_:
-        case "file":
-            return CopyFileToRemote(source, config.server, dest)
-        case "dir":
-            return CopyDirToRemote(source, config.server, dest)
-        case "content":
-            return CopyDirContentToRemote(source, config.server, dest)
-        case _:
-            raise RuntimeError
-
-
 @task
-def copy(ctx):
+def update_content(ctx):
     """Copy files on the server"""
-    commands = []
-    for type_, source, dest in config.copy:
-        dest = Path(dest)
-        commands.extend(
-            [
-                ExecuteOnHost(config.server, EnsureDirectoryExists(dest)),
-                copy_command_from_config(type_, source, dest),
-            ]
-        )
-    for type_, source, dest in config.copy_remote:
-        commands.append(
-            ExecuteOnHost(config.server, Sudo(CopyDirContent(source, dest)))
-        )
+    commands = [
+        ExecuteOnHost(
+            config.REMOTE, EnsureDirectoryExists(config.CONTENT_REMOTE_TEMP_DIR)
+        ),
+        CopyDirContentToRemote(
+            config.CONTENT_LOCAL_DIR, config.REMOTE, config.CONTENT_REMOTE_TEMP_DIR
+        ),
+        ExecuteOnHost(
+            config.REMOTE, Sudo(EnsureDirectoryExists(config.CONTENT_REMOTE_FINAL_DIR))
+        ),
+        ExecuteOnHost(
+            config.REMOTE,
+            Sudo(
+                CopyDirContent(
+                    config.CONTENT_REMOTE_TEMP_DIR, config.CONTENT_REMOTE_FINAL_DIR
+                )
+            ),
+        ),
+    ]
     for command in commands:
         run_command(ctx, command)
 
 
-# @task
-# def terminate(ctx):
-#     """Terminate server"""
-#     command = ExecuteOnHost(config.server, DockerComposeDown(config.compose))
-#     run_command(ctx, command)
+@task
+def update_config(ctx):
+    """Copy files on the server"""
+    commands = [
+        ExecuteOnHost(
+            config.REMOTE,
+            EnsureDirectoryExists(Path(config.SERVER_CONFIG_FILE_REMOTE_TEMP).parent),
+        ),
+        CopyFileToRemote(
+            config.SERVER_CONFIG_FILE_LOCAL,
+            config.REMOTE,
+            config.SERVER_CONFIG_FILE_REMOTE_TEMP,
+        ),
+        ExecuteOnHost(
+            config.REMOTE,
+            Sudo(
+                CopyFile(
+                    config.SERVER_CONFIG_FILE_REMOTE_TEMP,
+                    config.SERVER_CONFIG_FILE_REMOTE_FINAL,
+                )
+            ),
+        ),
+        ExecuteOnHost(config.REMOTE, Sudo(NginxReload())),
+    ]
 
-
-# @task
-# def start(ctx):
-#     """Start server"""
-#     command = ExecuteOnHost(config.server, DockerComposeUp(config.compose))
-#     run_command(ctx, command)
+    for command in commands:
+        run_command(ctx, command)
